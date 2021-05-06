057ab7b47a76f455910ec218c03c4b8b3a334d3d
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-05-06 13:39:03 +0200

    added deleting selected swimmer

--- app/Deleter.py
+++ app/Deleter.py
@@ -1,17 +1,14 @@
+from Worker import Worker
 
-class Deleter():
-    
-    def __init__(self, connection, cursor, checker):
-        self.connection = connection
-        self.cursor = cursor
-        self.checker = checker
+
+class Deleter(Worker):
 
     def run(self):
         print('\n\nWhat do you want to delete?')
         print('1. All swimmers that have 0 starts')
         print('2. All starts older than one year')
         print('3. Coaches who are coaches to 0 swimmers')
-        # TODO usuwanie wybranego zawodnika
+        print('4. Selected swimmer')
         print('Anything else to exit')
         choice = input()
         if choice == '1':
@@ -20,6 +17,8 @@ class Deleter():
             self._delete_starts()
         elif choice == '3':
             self._delete_coaches()
+        elif choice == '4':
+            self._delete_selected_swimmer()
         else:
             return
         
@@ -28,7 +27,7 @@ class Deleter():
         DELETE FROM Swimmers WHERE SwimmerID NOT IN (SELECT DISTINCT SwimmerID FROM Starts);
         """)
         self.connection.commit()
-        print('Deleted ' + str(self.cursor.rowcount) + ' rows')
+        print('Deleted ' + str(self.cursor.rowcount) + ' row(s)')
 
     def _delete_starts(self):
         import datetime
@@ -38,11 +37,29 @@ class Deleter():
         DELETE FROM Starts WHERE StartDate < ?
         """, one_year_ago.strftime('%Y-%m-%d'))
         self.connection.commit()
-        print('Deleted ' + str(self.cursor.rowcount) + ' rows')
+        print('Deleted ' + str(self.cursor.rowcount) + ' row(s)')
 
     def _delete_coaches(self):
         self.cursor.execute("""
             DELETE FROM Coaches WHERE CoachID NOT IN (SELECT DISTINCT CoachID FROM Swimmers);
             """)
         self.connection.commit()
-        print('Deleted ' + str(self.cursor.rowcount) + ' rows')
+        print('Deleted ' + str(self.cursor.rowcount) + ' row(s)')
+
+
+    def _delete_selected_swimmer(self):
+        last_name = input('Swimmer last name: ')
+        self.cursor.execute("""
+        --sql
+        SELECT SwimmerID, LastName, FirstName FROM Swimmers
+        WHERE Lastname = ?;""", last_name)
+        print('')
+        self._pretty_print()
+        print('')
+        swimmer_id = input('Type id of a swimmer to delete: ')
+        self.cursor.execute("""
+            DELETE FROM Swimmers WHERE SwimmerID=?;
+            """, swimmer_id)
+        self.connection.commit()
+        print('Deleted ' + str(self.cursor.rowcount) + ' row(s)')
+
